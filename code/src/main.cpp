66f0c91311edbe8191bda6438b6d2c22cfe98d56
Dena-lab <denis.majrich@gmail.com>
2024-08-30 20:09:52 +0200

    Tested the IO expander

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <Adafruit_MCP23X08.h>
+#include <Adafruit_MCP23X17.h>
 
-#define SOLENOID_PIN 2
+#define SOLENOID_PIN 10
+
+Adafruit_MCP23X17 mcp;
+
 
 void setup() {
   Serial.begin(115200);
   pinMode(SOLENOID_PIN, OUTPUT);
+
+   if (!mcp.begin_I2C()) {
+    Serial.println("MCP23X17 not found");
+    while (1);
+  }
+
+  // Set all mcp pins to input_pullup
+  for (uint8_t i = 0; i < 16; i++) {
+    mcp.pinMode(i, INPUT_PULLUP);
+  }
 }
 
 void loop() {
-  Serial.println("Solenoid ON");
-  digitalWrite(SOLENOID_PIN, HIGH);
-  delay(1000);
-  Serial.println("Solenoid OFF");
-  digitalWrite(SOLENOID_PIN, LOW);
-  delay(1000);
+
+  // Read all mcp pins
+  for (uint8_t i = 0; i < 16; i++) {
+    Serial.print("  Pin ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.print(mcp.digitalRead(i));
+  }
+Serial.println();
+delay(100);
 }
 
